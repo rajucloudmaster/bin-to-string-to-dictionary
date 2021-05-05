c3d1015ef6d6235876991d4ddae1a2af46fed22d
rajucloudmaster <59248004+rajucloudmaster@users.noreply.github.com>
2021-05-05 23:29:37 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,6 @@
+
+
+
 data1 = b'{"Data":[],"Total":140,"AggregateResults":null,"Errors":null}'
 str = data1.decode("UTF-8")
 
@@ -5,4 +8,4 @@ Dict = dict((x.strip(), y.strip())
             for x, y in (element.split(':')
                          for element in str.split(',')))
 
-print("the Total value  is-------**************************************-----> ", Dict.get('"Total"'))
+print("the Total value  is: ", Dict.get('"Total"'))
